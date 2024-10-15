f2dd240931bd31396cc6e8106dd7a198e65b5f60
Amaldev <amaldev.psn@gmail.com>
2024-10-15 23:13:21 +0530

    playing

---
+++ playground.js
@@ -0,0 +1,19 @@
+/* 
+let element = document.createElement(component.selector);
+element.innerHTML = component.template;
+this.root.appendChild(element);
+
+const element = document.querySelector(this.selector);
+
+const app = new Framework(document.getElementById('app'));
+app.registerComponent(new MyComponent()); */
+debugger;
+let apppElement = document.getElementById('app');
+let element = document.createElement("amal");
+element.innerHTML = '<h1>Hiii</h1>';
+apppElement.appendChild(element);
+
+const amal = document.querySelector('amal');
+
+
+
